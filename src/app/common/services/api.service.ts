bf22be308a0e6bc003375c7821b5bef3c1c0fa64
mikhail <maslakov.rostislav96@gmail.com>
2024-11-14 14:23:13 +0300

    changed static params to httpParams

--- src/app/common/services/api.service.ts
+++ src/app/common/services/api.service.ts
@@ -1,20 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { GetWeatherResponse } from '../interfaces/get-weather-response.interface'
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {GetWeatherResponse} from '../interfaces/get-weather-response.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiKey: string = '67e3c998ce59e08da1d0d97b3b6dee11'
+  private apiKey: string = '67e3c998ce59e08da1d0d97b3b6dee11';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-  constructor(private http: HttpClient) { }
-
-  getWeather(city: string): Observable<GetWeatherResponse> {
-    const apiUrl: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`;
-
-    return this.http.get<GetWeatherResponse>(apiUrl)
+  constructor(private http: HttpClient) {
   }
 
+  getWeather(city: string): Observable<GetWeatherResponse> {
+    const params = new HttpParams()
+      .set('q', city)
+      .set('units', 'metric')
+      .set('appid', this.apiKey);
+
+    return this.http.get<GetWeatherResponse>(this.apiUrl, {params})
+  }
 }
