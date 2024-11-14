e039154cf531876f81b1b5422cad0a9375d01e84
mikhail <maslakov.rostislav96@gmail.com>
2024-11-14 12:26:41 +0300

    added login form

---
+++ src/app/dashboard/login/login.component.ts
@@ -0,0 +1,49 @@
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+  errorMessage = '';
+
+  constructor(private http: HttpClient, private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  login() {
+    const {username, password} = this.loginForm.value;
+
+    this.http.post('https://fakestoreapi.com/auth/login', {
+      username: username,
+      password: password,
+    }).subscribe({
+      next: (response: any) => {
+        localStorage.setItem('token', response.token);
+
+        if (username === 'mor_2314') {
+          localStorage.setItem('isAdmin', 'true');
+        }
+
+        this.router.navigate(['/dashboard']);
+      },  
+      error: () => {
+        this.errorMessage = 'Invalid credentials';
+      }
+    });
+  }
+}
