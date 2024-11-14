50ccfd0e9e91867072cec9c7e546d401cd09e143
mikhail <maslakov.rostislav96@gmail.com>
2024-11-14 16:42:49 +0300

    added server verification

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
-import { ApiService } from '../common/services/api.service';
-import { GetWeatherResponse } from '../common/interfaces/get-weather-response.interface';
-import { catchError, tap } from 'rxjs';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {ApiService} from '../common/services/api.service';
+import {GetWeatherResponse} from '../common/interfaces/get-weather-response.interface';
+import {catchError, tap} from 'rxjs';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {JsonPipe, NgForOf, NgIf} from '@angular/common';
-import { DivDataComponent } from './div-data/div-data.component';
+import {DivDataComponent} from './div-data/div-data.component';
 import {RouterLink} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -24,23 +25,24 @@ export class DashboardComponent {
   isAdmin: boolean = localStorage.getItem('isAdmin') === 'true';
   selectedCityImage: string | null = null;
   popularCities = [
-    { name: 'Paris', imageUrl: 'http://localhost:3000/paris' },
-    { name: 'London', imageUrl: 'http://localhost:3000/london' },
-    { name: 'Moscow', imageUrl: 'http://localhost:3000/moscow' },
-    { name: 'Warsaw', imageUrl: 'http://localhost:3000/warsaw' },
-    { name: 'Berlin', imageUrl: 'http://localhost:3000/berlin' },
-    { name: 'Lisbon', imageUrl: 'http://localhost:3000/lisbon' },
-    { name: 'Rome', imageUrl: 'http://localhost:3000/rome' },
-    { name: 'Sydney', imageUrl: 'http://localhost:3000/sydney' },
-    { name: 'Tokyo', imageUrl: 'http://localhost:3000/tokyo' },
+    {name: 'Paris', imageUrl: 'http://localhost:3000/paris'},
+    {name: 'London', imageUrl: 'http://localhost:3000/london'},
+    {name: 'Moscow', imageUrl: 'http://localhost:3000/moscow'},
+    {name: 'Warsaw', imageUrl: 'http://localhost:3000/warsaw'},
+    {name: 'Berlin', imageUrl: 'http://localhost:3000/berlin'},
+    {name: 'Lisbon', imageUrl: 'http://localhost:3000/lisbon'},
+    {name: 'Rome', imageUrl: 'http://localhost:3000/rome'},
+    {name: 'Sydney', imageUrl: 'http://localhost:3000/sydney'},
+    {name: 'Tokyo', imageUrl: 'http://localhost:3000/tokyo'},
   ];
 
   weatherObj: GetWeatherResponse | null = null;
 
-  constructor(private readonly api: ApiService) {}
+  constructor(private readonly api: ApiService, private http: HttpClient) {
+  }
 
 
-  onGetData(){
+  onGetData() {
     this.isLoading = true;
 
     const cityMatch = this.popularCities.find(city => city.name.toLowerCase() === this.cityControl.value.toLowerCase());
@@ -61,10 +63,26 @@ export class DashboardComponent {
     this.cityControl.reset();
   }
 
-  onSelectCity(cityName: string, imageUrl: string) {
+  onSelectCity(cityName: string) {
     this.cityControl.setValue(cityName);
-    this.selectedCityImage = imageUrl;
     this.onGetData();
+    this.http.get(`http://localhost:3000/${cityName.toLowerCase()}`, {
+      headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`},
+      responseType: 'blob' // если требуется загрузить изображение как Blob
+    }).subscribe({
+      next: (response) => {
+        // Обработка успешного получения изображения
+
+        this.selectedCityImage = URL.createObjectURL(response);
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          console.error('Unauthorized access');
+          // Отобразить сообщение о недоступности ресурса
+          this.selectedCityImage = null;
+        }
+      }
+    })
   }
 
 }
